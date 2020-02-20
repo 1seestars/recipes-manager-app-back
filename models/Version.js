36592670f455802be670f244ebb6b325644cc33d
iseestars <edelweisskaty@gmail.com>
2020-02-20 16:54:09 +0200

    init

---
+++ models/Version.js
@@ -0,0 +1,9 @@
+const { Schema } = require("mongoose")
+
+const versionSchema = new Schema(
+    {   
+        description: String
+    }
+)
+
+module.exports = { versionSchema }
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { RecipeModel } = require('./models/Recipe')
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get("/recipes", async (request, response) => {
+    const recipes = await RecipeModel.find({})
+    return response.json(recipes)
+})
+
+app.post("/recipe", async (request, response) => {
+    const { name, description } = request.body
+    const newItem = await RecipeModel.create({ name, createdAt: Date.now(), versions: [{ description }] })
+    return response.json(newItem)
+})
+
+app.put("/recipe/:id", async (request, response) => {
+    const { body: { description }, params: { id } } = request
+    const recipe = await RecipeModel.findOne({ _id: id })
+    await recipe.versions.push({ description })
+    await recipe.save()
+    return response.json(recipe)
+})
+
+app.delete("/recipe/:id", async (request, response) => {
+    const { id } = request.params
+    const recipe = await RecipeModel.deleteOne({ _id: id })
+    recipe.message = "Recipe has been removed"
+    return response.json(recipe)
+})
+
+app.delete("/recipes", async (request, response) => {
+    const recipes = await RecipeModel.deleteMany({})
+    recipes.message = "All recipes have been removed"
+    return response.json(recipes)
+})
+
+app.listen(4000, () => {
+    console.log("Server is runnig")
+})
